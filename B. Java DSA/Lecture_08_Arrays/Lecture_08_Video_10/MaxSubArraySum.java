b8a8e22288d5c21d349966fdf4811fd930582a71
PRASHANT SAINI <Prashant.saini.3150@gmail.com>
2025-02-18 23:59:50 +0530

    Array

---
+++ B. Java DSA/Lecture_08_Arrays/Lecture_08_Video_10/MaxSubArraySum.java
@@ -0,0 +1,32 @@
+
+public class MaxSubArraySum {
+
+    public static void maxSubArraySum(int numbers[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE; //+infinity
+
+        for (int i = 0; i < numbers.length; i++) { //outer loop for each number
+            for (int j = i; j < numbers.length; j++) { //inner loop for each number
+                currSum = 0;
+                for (int k = i; k <= j; k++) { //inner loop for print subarray values
+
+                    //subarray sum
+                    currSum += numbers[k];
+                }
+                System.out.println(currSum);
+                //update maxSum if current sum is greater
+                if(maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+            System.out.println("-------------------");
+        }
+        //print the max subarray sum
+        System.out.println("Max Sum : " + maxSum);
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {1, -2, 6, -1, 3};
+        maxSubArraySum(numbers);
+    }
+}
---
+++ B. Java DSA/Lecture_08_Arrays/Lecture_08_Video_10/MaxSubArraySum1.java
@@ -0,0 +1,41 @@
+public class MaxSubArraySum1 {
+    public static void maxSubArraySum(int numbers[]) {
+        int ts = 0;
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE; //- infinity
+        int minSum = Integer.MAX_VALUE; //+ infinity
+
+        for (int i = 0; i < numbers.length; i++) { //outer loop for each number
+            for (int j = i; j < numbers.length; j++) { //print //inner
+                currSum = 0;
+                System.out.print("[");
+                for (int k = i; k <= j; k++) {
+                    System.out.print(numbers[k] + (k == j ? "" : ", "));//subarrays
+                    
+                    //subarray sum
+                    currSum += numbers[k];
+                }
+                System.out.println("] -> Sum: " + currSum); // Print subarray and its sum
+                //update maxSum if current sum is greater
+                if(maxSum < currSum) {
+                    maxSum = currSum;
+                } 
+                if (currSum < minSum) {
+                    minSum = currSum;
+                }                
+                ts++; //total subarrays printed
+                // System.out.println();
+            }
+            System.out.println("-------------------");
+        }
+        System.out.println("Max Sum : " + maxSum);
+        System.out.println("Min Sum : " + minSum);
+        // Print the total number of subarrays and max sum
+        System.out.print("Total subarrays printed: " + ts); //total subarrays printed: 15
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {1, -2, 6, -1, 3};
+        maxSubArraySum(numbers);
+    }
+}
